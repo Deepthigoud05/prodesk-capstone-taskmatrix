a7938a3eb518d05841d95296c8a0e9b644bb52c2
Your Name <you@example.com>
2026-07-13 11:22:13 +0530

    Added user registration API

---
+++ server/controllers/authController.js
@@ -0,0 +1,52 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Register User
+const registerUser = async (req, res) => {
+    try {
+
+        const { name, email, password } = req.body;
+
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        // Hash Password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create User
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        await user.save();
+
+        // Generate JWT
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        res.status(201).json({
+            message: "Registration Successful",
+            token
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Server Error"
+        });
+    }
+};
+
+module.exports = {
+    registerUser
+};
---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+const authRoutes = require("./routes/authRoutes");
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("TaskMatrix API is running...");
+});
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
